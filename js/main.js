791b5466f11c772bbe66eeb7649622b62f84bd46
Jason Lyons <jasonlyons@Jasons-MacBook-Pro.local>
2015-02-10 11:59:14 -0500

    update

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)) {
 }
 
 function onDeviceReady() {
+	initPushwoosh();
+	
 	app_log("onDeviceReady");
 	isLoggedIn();
 }
@@ -33,4 +35,44 @@ var app = {
         }
     }
 }
+
+
+function initPushwoosh() {
+    var pushNotification = window.plugins.pushNotification;
+ 
+    //set push notification callback before we initialize the plugin
+    document.addEventListener('push-notification', function(event) {
+                                //get the notification payload
+                                var notification = event.notification;
+ 
+                                //display alert to the user for example
+                                alert(notification.aps.alert);
+                               
+                                //clear the app badge
+                                pushNotification.setApplicationIconBadgeNumber(0);
+                            });
+ 
+    //initialize the plugin
+    pushNotification.onDeviceReady({pw_appid:"A4717-E85B7"});
+     
+    //register for pushes
+    pushNotification.registerDevice(
+        function(status) {
+            var deviceToken = status['deviceToken'];
+            console.warn('registerDevice: ' + deviceToken);
+        },
+        function(status) {
+            console.warn('failed to register : ' + JSON.stringify(status));
+            alert(JSON.stringify(['failed to register ', status]));
+        }
+    );
+     
+    //reset badges on app start
+    pushNotification.setApplicationIconBadgeNumber(0);
+}
 	
+document.addEventListener('push-notification', function(event) {
+             var notification = event.notification;
+             alert(notification.aps.alert);
+             pushNotification.setApplicationIconBadgeNumber(0);
+});	

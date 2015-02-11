3f826e3e541d82dc7b8fe9afc3d8030a6530feac
Jason Lyons <jasonlyons@Jasons-MacBook-Pro.local>
2015-02-11 14:16:15 -0500

    update

--- js/main.js
+++ js/main.js
@@ -77,7 +77,9 @@ function initPushwoosh() {
 	
 document.addEventListener('push-notification', function(event) {
              var notification = event.notification;
-             alert(notification.aps.alert);
+             app_log(notification);
+			 
+			 alert(notification.aps.alert);
              pushNotification.setApplicationIconBadgeNumber(0);
 });	
 
